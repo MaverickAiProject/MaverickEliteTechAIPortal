cdefe28cd4d3d4e4f49c68bd90dfe4c22565e87e
Maverick <maverick.aiproject@gmail.com>
2024-12-30 13:01:02 +0530

    responsiveness

--- src/components/Menubar.jsx
+++ src/components/Menubar.jsx
@@ -4,9 +4,10 @@ import { images } from '../assets/images';
 import { GiHamburgerMenu } from "react-icons/gi";
 import { ImCross } from "react-icons/im";
 import { Context } from '../context/Context';
+import { useNavigate } from 'react-router-dom';
 
 function Menubar() {
-
+    const navigate = useNavigate()
     const { setNavOpen, navOpen } = useContext(Context)
 
     const toggleNavbar = () => {
@@ -28,7 +29,7 @@ function Menubar() {
                 }
             </div>
             <img src={images.logo_purple_transparent} alt="" className='h-10' />
-            <FaRegCircleUser size={30} />
+            <FaRegCircleUser size={30} onClick={() => navigate('/settings')} className='cursor-pointer hover:text-primary transition-all duration-200' />
         </div>
     )
 }
