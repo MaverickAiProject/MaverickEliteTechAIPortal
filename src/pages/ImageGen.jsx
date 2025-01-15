e7f2fa274e3e5cb89371375dc3bd96adec3ae2b6
Maverick <maverick.aiproject@gmail.com>
2025-01-15 15:56:46 +0530

    updates

--- src/pages/ImageGen.jsx
+++ src/pages/ImageGen.jsx
@@ -7,7 +7,6 @@ import aiSec from '../assets/ai-sec.jpg'
 import pg from '/progress.gif'
 
 function ImageGen() {
-    console.log("API Key:", import.meta.env.VITE_HUGGING_FACE_IMAGE_GENERATION_API);
 
     const [prompt, setPrompt] = useState("");
     const [imageUrl, setImageUrl] = useState(null);
@@ -148,5 +147,3 @@ function ImageGen() {
 }
 
 export default ImageGen
-
-
