ee06cf04787917e978e4aba14ad5de48c385a91b
Maverick <maverick.aiproject@gmail.com>
2025-01-15 16:06:07 +0530

    updates

--- src/pages/ImageGen.jsx
+++ src/pages/ImageGen.jsx
@@ -19,7 +19,7 @@ function ImageGen() {
                 "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev",
                 {
                     headers: {
-                        Authorization: "Bearer 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",
+                        Authorization: "Bearer " + import.meta.env.VITE_HUGGING_FACE_IMAGE_GENERATION_API,
                         "Content-Type": "application/json",
                     },
                     method: "POST",
