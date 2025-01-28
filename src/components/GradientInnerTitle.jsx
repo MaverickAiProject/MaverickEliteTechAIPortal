a9d88e65c3882d8e396cb9d6c8499fa7392eb70e
Maverick <maverick.aiproject@gmail.com>
2025-01-28 11:53:44 +0530

    design

--- src/components/GradientInnerTitle.jsx
+++ src/components/GradientInnerTitle.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function GradientInnerTitle({ title, description, icon }) {
     return (
-        <div className='flex gap-3 items-center justify-center md:px-2 px-4'>
+        <div className='flex items-center justify-center md:px-2 px-4'>
             <img src={icon} alt="" className='h-12 rounded shadow-sm' />
             <div className='w-full px-4'>
                 <h1 className='font-semibold md:text-3xl text-xl md:mb-0 mb-1'>{title}</h1>
