8737f804472f299e4552d8b0142bedf2451317bc
Maverick <maverick.aiproject@gmail.com>
2025-01-28 11:51:28 +0530

    updates

--- src/components/GradientInnerTitle.jsx
+++ src/components/GradientInnerTitle.jsx
@@ -4,9 +4,9 @@ function GradientInnerTitle({ title, description, icon }) {
     return (
         <div className='flex gap-3 items-center justify-center md:px-2 px-4'>
             <img src={icon} alt="" className='h-12 rounded shadow-sm' />
-            <div className='md:w-full w-[80%]'>
+            <div className='w-full px-4'>
                 <h1 className='font-semibold md:text-3xl text-xl md:mb-0 mb-1'>{title}</h1>
-                <p className='md:text-base text-base'>{description}</p>
+                <p className='md:text-base text-xs'>{description}</p>
             </div>
         </div>
     )
