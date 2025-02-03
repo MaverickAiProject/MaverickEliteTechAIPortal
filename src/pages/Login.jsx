5d9784bf5dd1ae456952c12f5f4a267ce071e950
Maverick <maverick.aiproject@gmail.com>
2025-02-03 11:30:37 +0530

    Forgot password implemented

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,22 @@
-import React, { useState, useContext } from "react";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase.config.js";
+import React, { useState, useContext, useEffect } from "react";
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "../firebase.config.js";
 import { Link, useNavigate } from "react-router-dom";
 import { Context } from "../context/Context";
 import ReactLoading from 'react-loading';
 import PolicyLinks from "../components/PolicyLinks.jsx";
 import LogoVideo from "../components/LogoVideo.jsx";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState("");
+
+    const [resetMailSent, setResetMailSent] = useState(false)
+    const [timer, setTimer] = useState(0);
+
     const { setAuthorizedUser, fetchUserData } = useContext(Context);
     const navigate = useNavigate();
 
@@ -41,6 +46,46 @@ function Login() {
             setLoading(false)
         }
     };
+
+    const handleForgotPassword = async () => {
+        if (!email) {
+            setError("Please enter your email to send password reset link.")
+            return;
+        }
+        try {
+
+            const q = query(collection(db, 'Users'), where("email", "==", email))
+            const querySnapshot = await getDocs(q)
+
+            if (querySnapshot.empty) {
+                setError("This email is not registered. Please check again.");
+                return;
+            }
+
+            await sendPasswordResetEmail(auth, email)
+            setError('');
+            setResetMailSent(true);
+            setTimer(30);
+
+        } catch (err) {
+            if (err.code === "auth/user-not-found") {
+                setError("This email is not registered. Please check again.");
+            } else if (err.code === "auth/invalid-email") {
+                setError(" Please enter a valid email address.");
+            } else {
+                setError("Something went wrong. Try again later. " + err);
+            }
+        }
+    }
+
+    useEffect(() => {
+        if (timer > 0) {
+            const interval = setInterval(() => {
+                setTimer((prev) => prev - 1)
+            }, 1000);
+            return () => clearInterval(interval);
+        }
+    }, [timer])
 
     return (
         <div className="flex items-center justify-center min-h-screen bg-dashboardBg ">
@@ -82,7 +127,44 @@ function Login() {
                         }
                     </button>
                 </form>
-                <div className="text-center mt-6">
+                {error && (
+                    <p className="mt-4 text-sm text-red-600 dark:text-red-400 text-center">
+                        {error}
+                    </p>
+                )}
+
+                {/* Reset Password function */}
+                {!resetMailSent
+                    ? <div
+                        className="text-greyText text-right mt-2 w-full"
+                        onClick={handleForgotPassword}
+                    >
+                        <span className="border-b border-transparent w-fit hover:border-greyText transition-all ease-in-out duration-300 cursor-pointer"> Forget password?</span>
+                    </div>
+
+                    : <div
+                        className="text-greyText text-center mt-2 w-full"
+                    ><div>
+                            <p>
+                                Password reset link has been sent to your email.
+                            </p>
+
+                            <p className="text-purpleText">
+                                {timer > 0
+                                    ? `Resend mail after ${timer} seconds.`
+                                    : <button
+                                        onClick={handleForgotPassword}
+                                        className="text-purpleText font-semibold hover:underline"
+                                    >
+                                        Resend Email
+                                    </button>
+                                }
+                            </p>
+                        </div>
+                    </div>
+                }
+
+                <div className="text-center mt-5">
                     <p>
                         Don’t have an account?{" "}
                         <Link to={"/signup"} className="text-purpleText font-semibold">
@@ -90,16 +172,12 @@ function Login() {
                         </Link>
                     </p>
                 </div>
-                {error && (
-                    <p className="mt-4 text-sm text-red-600 dark:text-red-400 text-center">
-                        {error}
-                    </p>
-                )}
+
                 <div>
                     <PolicyLinks />
                 </div>
             </div>
-        </div>
+        </div >
     );
 }
 
