f13c7924eb0ee4414952b964b0d5fd29b315b16a
Maverick <maverick.aiproject@gmail.com>
2025-02-07 10:38:53 +0530

    updates

--- src/components/dashboard/GradientCard.jsx
+++ src/components/dashboard/GradientCard.jsx
@@ -6,11 +6,11 @@ function GradientCard({ gradient, link, image, otherText, heading }) {
 
     return (
         <div
-            className={`flex items-center px-3 py-3 justify-center h-28 sm:h-36 gap-3 ${gradient} rounded-lg shadow-md cursor-pointer relative bottom-0 hover:bottom-1 ease-in-out transition-all duration-200 hover:shadow-lg`}
+            className={`flex items-center px-3 py-5 sm:py-3 justify-center sm:h-36 gap-3 ${gradient} rounded-lg shadow-md cursor-pointer relative bottom-0 hover:bottom-1 ease-in-out transition-all duration-200 hover:shadow-lg`}
             onClick={() => navigate(`/${link}`)}
         >
             <div className='w-[20%] py-3'>
-                <img src={image} alt="text" className='w-full max-w-20 ' />
+                <img src={image} alt="text" className='w-full sm:max-w-20 max-w-16' />
             </div>
             <div className='flex flex-col justify-center sm:gap-1'>
                 <h2 className='xl:text-3xl md:text-2xl text-xl font-bold text-white'>{heading}</h2>
